041168fd33e70f5d87e459ee1810f20de454b3f2
Marko Troskot <marko.troskot@styliff.com>
2023-02-10 11:41:31 +0100

    project init

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,22 @@
+import React, { HTMLProps } from "react";
+import styles from "./Dashboard.module.css";
+import classNames from "classnames";
+
+interface DashboardProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const Dashboard: React.FC<DashboardProps & HTMLProps<HTMLDivElement>> = ({
+  children,
+  className,
+  ...restProps
+}) => {
+  return (
+    <div className={classNames(styles.container, className)} {...restProps}>
+      Hello World
+    </div>
+  );
+};
+
+export default React.memo(Dashboard);
